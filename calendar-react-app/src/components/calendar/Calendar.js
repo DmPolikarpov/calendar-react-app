8747b974017e6650975c113e11f212f6526ac74c
Dmitrii Polikarpov <mazaxist2@mail.ru>
2021-11-19 01:59:54 +0300

    week days were added into the calendar.

--- calendar-react-app/src/components/calendar/Calendar.js
+++ calendar-react-app/src/components/calendar/Calendar.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './Calendar.css';
+import constants from '../../configs/constants';
 
 const Calendar = () => {
     return (
-        <div className="calendar"></div>
+        <div className="calendar">
+            <div className="weekdays-container">
+                {
+                    constants.weekdays_short.map(day => {
+                        return <div>{day}</div>
+                    })
+                }
+            </div>
+        </div>
     )
 }
 
---
+++ calendar-react-app/src/configs/constants.js
@@ -0,0 +1,6 @@
+let constants = {
+    "weekdays": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+    "weekdays_short": ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]
+}
+
+export default constants;
