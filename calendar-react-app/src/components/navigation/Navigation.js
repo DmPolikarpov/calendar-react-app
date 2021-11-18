3e65ec8389f73cefda79948d19d826c12842c2eb
Dmitrii Polikarpov <mazaxist2@mail.ru>
2021-11-19 01:21:05 +0300

    component of navigation and calendar was created.

---
+++ calendar-react-app/src/components/navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navigation.css';
+
+const Navigation = () => {
+    return (
+        <nav className="nav-bar">
+            <p>Sign Up</p>
+            <p>Log In</p>
+        </nav>
+    )
+}
+
+export default Navigation;
